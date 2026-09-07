f79a9934c3baca5fd84bd43097820295bf5aba79
meoww506 <07240124.sherubtse@rub.edu.bt>
2026-09-07 21:54:49 +0600

    Unit III: MVC scaffolding, partials, about page, restaurants, menu, and ordering

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const homeController = require('../controllers/homeController');
+const aboutController = require('../controllers/aboutController');
+
+router.get('/', homeController.getHome);
+router.get('/about', aboutController.getAbout);
+const menuController = require('../controllers/menuController');
+const orderController = require('../controllers/orderController');
+
+router.post('/orders', orderController.createOrder);
+
+
+router.get('/menu', menuController.getMenu);
+
+
+module.exports = router;
